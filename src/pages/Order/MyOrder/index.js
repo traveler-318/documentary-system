fef882ee966f15d2a5f0dfa3a01d5023039e5cb2
123 <2623220561@qq.com>
2020-08-30 23:12:58 +0800

    测试提交发布

--- src/pages/Order/MyOrder/index.js
+++ src/pages/Order/MyOrder/index.js
@@ -17,13 +17,7 @@ class AllOrdersList extends PureComponent {
           selectedRowKeys:[],
           orderStatus:[
             {name:"全部",key:null},
-            {name:"待审核",key:1},
-            {name:"已审核",key:2},
-            {name:"已发货",key:3},
-            {name:"在途中",key:4},
-            {name:"已签收",key:5},
-            {name:"跟进中",key:6},
-            {name:"已激活",key:7}
+            {name:"待审核",key:1}
           ]
         };
       }
