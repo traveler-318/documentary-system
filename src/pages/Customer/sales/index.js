628866526e0c2a04af4d43ef7b00c73a5e3dbb5a
danfuman <1169694155@qq.com>
2020-09-16 22:43:09 +0800

    销售管理列表

---
+++ src/pages/Customer/sales/index.js
@@ -0,0 +1,288 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'dva';
+import {
+  Button,
+  Col,
+  Form,
+  Input,
+  Row,
+  Select,
+  DatePicker,
+  Divider,
+  Dropdown,
+  Menu,
+  Icon,
+  Switch,
+  Modal,
+  message,
+} from 'antd';
+import { formatMessage, FormattedMessage } from 'umi/locale';
+import router from 'umi/router';
+import Panel from '../../../components/Panel';
+import Grid from '../../../components/Sword/Grid';
+
+import { getList, getRemove, getSubmit,getUrl } from '../../../services/newServices/logistics';
+
+
+const FormItem = Form.Item;
+const { RangePicker } = DatePicker;
+
+@connect(({ globalParameters }) => ({
+  globalParameters,
+}))
+@Form.create()
+class AuthorityList extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data:{},
+      loading:false,
+      params:{
+        size:10,
+        current:1
+      }
+    };
+  }
+  // ============ 初始化数据 ===============
+
+  componentWillMount() {
+    this.getDataList();
+  }
+
+  getDataList = () => {
+    const {params} = this.state;
+    this.setState({
+      loading:true
+    })
+    getList(params).then(res=>{
+      this.setState({
+        loading:false
+      })
+      this.setState({
+        data:{
+          list:res.data.records,
+          pagination:{
+            current: res.data.current,
+            pageSize: res.data.size,
+            total: res.data.total
+          }
+        }
+      })
+    })
+  }
+
+  // ============ 查询 ===============
+  handleSearch = params => {
+    this.setState({
+      params
+    },()=>{
+      this.getDataList();
+    })
+  };
+
+  // ============ 查询表单 ===============
+
+  renderSearchForm = onReset => {
+
+  };
+
+  // ============ 删除 ===============
+
+  handleClick = ( id) => {
+    const params={
+      ids: id
+    }
+    const refresh = this.getDataList;
+    Modal.confirm({
+      title: '删除确认',
+      content: '确定删除该条记录?',
+      okText: '确定',
+      okType: 'danger',
+      cancelText: '取消',
+      onOk() {
+        getRemove(params).then(resp => {
+          if (resp.success) {
+            message.success(resp.msg);
+            refresh()
+          } else {
+            message.error(resp.msg || '删除失败');
+          }
+        });
+      },
+      onCancel() {
+
+      },
+    });
+  };
+
+  // ============ 授权 ===============
+  activeAuthorization = ( id) => {
+    getUrl(id).then(res => {
+      window.open(JSON.parse(res.data).data.url)
+    });
+  };
+
+
+  // ============ 修改默认开关 =========
+
+  onStatus = (value,key) => {
+    const refresh = this.getDataList;
+    const data= value === 0 ? 1 : 0;
+    const params = {
+      id:key.id,
+      status:data
+    };
+    Modal.confirm({
+      title: '修改确认',
+      content: '是否要修改该状态??',
+      okText: '确定',
+      okType: 'danger',
+      cancelText: '取消',
+      async onOk() {
+        getSubmit(params).then(resp=>{
+          if (resp.success) {
+            message.success(resp.msg);
+            refresh()
+          } else {
+            message.error(resp.msg || '修改失败');
+          }
+        })
+      },
+      onCancel(){},
+    });
+  };
+
+  // 修改数据
+  handleEdit = (row) => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: `globalParameters/setDetailData`,
+      payload: row,
+    });
+    router.push('/logistics/authority/edit');
+  };
+
+  onSelectRow = rows => {
+    console.log(rows,"rows")
+    this.setState({
+      selectedRows: rows,
+    });
+  };
+
+  renderLeftButton = () => (
+    <>
+      数据列表
+    </>
+  );
+
+  renderRightButton = () => (
+    <>
+      <Button type="primary" onClick={()=>{router.push(`/logistics/authority/add`);}}>分组</Button>
+      <Button type="primary" onClick={()=>{router.push(`/logistics/authority/add`);}}>充值</Button>
+      <Button type="primary" onClick={()=>{router.push(`/logistics/authority/add`);}}>添加</Button>
+    </>
+  );
+
+  render() {
+    const {
+      form,
+    } = this.props;
+
+    const {data,loading} = this.state;
+
+    const columns = [
+      {
+        title: '业务员姓名',
+        dataIndex: 'partnerId',
+        width: 100,
+      },
+      {
+        title: '分组',
+        dataIndex: 'partnerKey',
+        width: 100,
+      },
+      {
+        title: '手机号',
+        dataIndex: 'checkMan',
+        width: 150,
+      },
+      {
+        title: '客户数',
+        dataIndex: '',
+        width: 100,
+      },
+      {
+        title: '已履约',
+        dataIndex: '',
+        width: 100,
+      },
+      {
+        title: '待履约',
+        dataIndex: '',
+        width: 100,
+      },
+      {
+        title: '已逾期',
+        dataIndex: 'net',
+        width: 100,
+      },
+      {
+        title: '创建时间',
+        dataIndex: '',
+        width: 200,
+      },
+      {
+        title: '默认开关',
+        dataIndex: 'status',
+        width: 100,
+        render: (res,key) => {
+          return(
+            <Switch checked={res===1?true:false} onChange={() => this.onStatus(res,key)} />
+          )
+        },
+      },
+      {
+        title: '授权类型',
+        dataIndex: '',
+        width: 100,
+      },
+      {
+        title: '公众号通知',
+        dataIndex: '',
+        width: 100,
+      },
+      {
+        title: '操作',
+        key: 'operation',
+        fixed: 'right',
+        width: 150,
+        render: (res,row) => {
+          return(
+            <div>
+              <Divider type="vertical" />
+              <a onClick={()=>this.handleEdit(row)}>修改</a>
+              <Divider type="vertical" />
+              <a onClick={() => this.handleClick(res.id)}>聚合码</a></div>
+          )
+        },
+      },
+    ];
+    return (
+      <Panel>
+        <Grid
+          form={form}
+          onSearch={this.handleSearch}
+          renderSearchForm={this.renderSearchForm}
+          data={data}
+          onSelectRow={this.onSelectRow}
+          loading={loading}
+          columns={columns}
+          scroll={{ x: 1000 }}
+          renderLeftButton={this.renderLeftButton}
+          renderRightButton={this.renderRightButton}
+        />
+      </Panel>
+    );
+  }
+}
+export default AuthorityList;
