f9d5bf57adbbad1b528883d2d27e745ec37e5b18
谢文欣 <yf_xiewenxin@unisinsight.com>
2020-10-19 19:21:03 +0800

    样式修改
    样式修改

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -39,7 +39,7 @@ class HeaderView extends React.Component {
     if (isMobile || !fixedHeader || layout === 'topmenu') {
       return '100%';
     }
-    return collapsed ? 'calc(100% - 80px)' : 'calc(100% - 200px)';
+    return collapsed ? 'calc(100% - 80px)' : 'calc(100% - 180px)';
   };
 
   handleNoticeClear = type => {
