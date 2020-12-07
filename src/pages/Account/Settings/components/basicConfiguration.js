6f734cc34a740695627b99e94255bebe06b47179
谢文欣 <yf_xiewenxin@unisinsight.com>
2020-12-07 09:13:11 +0800

    id被删掉了

--- src/pages/Account/Settings/components/basicConfiguration.js
+++ src/pages/Account/Settings/components/basicConfiguration.js
@@ -244,83 +244,88 @@ class BaseView extends Component {
         <div className={styles.basicConfiguration}>
           <Row gutter={24}>
               <Col span={12}>
-              <FormItem
-                {...formItemLayout}
-                label={"头像"}
-              >
-                {getFieldDecorator('avatar', {
-                  // rules: [
-                  //   {
-                  //     required: true,
-                  //     message: "请上传头像",
-                  //   },
-                  // ],
-                })(
-                  <Upload
-                    name="file"
-                    listType="picture-card"
-                    className="avatar-uploader"
-                    showUploadList={false}
-                    beforeUpload={this.beforeUpload}
-                    onChange={this.handleChange}
-                    {...uploadProp}
-                  >
-                    {avatar ? (
-                      <img src={avatar} alt="avatar" style={{ width: '100%' }} />
-                    ) : (
-                        uploadButton
-                      )}
-                  </Upload>
-                )}
-              </FormItem>
-              <FormItem
-                {...formItemLayout}
-                label={"姓名"}
-              >
-                {getFieldDecorator('name', {
-                  rules: [
-                    {
-                      required: true,
-                      message: "请输入您的姓名!",
-                    },
-                  ],
-                })(<Input />)}
-              </FormItem>
-              <FormItem {...formItemLayout} label={"联系电话"}>
-                {getFieldDecorator('phone', {
-                  rules: [
-                    { required: true, validator: this.validatePhone },
-                  ],
-                })(<Input />)}
-              </FormItem>
-              <FormItem {...formItemLayout} label={"电子邮箱"}>
-                {getFieldDecorator('email', {
-                  rules: [
-                    {
-                      required: true,
-                      message: "请输入您的电子邮箱!",
-                    },
-                  ],
-                })(<Input />)}
-              </FormItem>
-              <FormItem {...formItemLayout} label={"微信绑定"}>
-                
-                {(openid && openid != '' && openid != null) ? 
-                  (<>
-                    <a onClick={()=>this.handleBinding('0')}>解绑</a>
-                    <Divider type="vertical" />
-                  </>):""
-                }
-                {(openid === '' || openid === null || !openid) ? 
-                  (<>
-                    <a onClick={()=>this.handleBinding('1')}>绑定</a>
-                    <Divider type="vertical" />
-                  </>):""
-                }
-                {(openid && openid != '' && openid != null) ? 
-                  (<a onClick={()=>this.handleTest()}>测试</a>):""
-                }
-              </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label={"头像"}
+                >
+                  {getFieldDecorator('avatar', {
+                    // rules: [
+                    //   {
+                    //     required: true,
+                    //     message: "请上传头像",
+                    //   },
+                    // ],
+                  })(
+                    <Upload
+                      name="file"
+                      listType="picture-card"
+                      className="avatar-uploader"
+                      showUploadList={false}
+                      beforeUpload={this.beforeUpload}
+                      onChange={this.handleChange}
+                      {...uploadProp}
+                    >
+                      {avatar ? (
+                        <img src={avatar} alt="avatar" style={{ width: '100%' }} />
+                      ) : (
+                          uploadButton
+                        )}
+                    </Upload>
+                  )}
+                </FormItem>
+                <FormItem
+                  {...formItemLayout}
+                  label={"姓名"}
+                >
+                  {getFieldDecorator('name', {
+                    rules: [
+                      {
+                        required: true,
+                        message: "请输入您的姓名!",
+                      },
+                    ],
+                  })(<Input />)}
+                </FormItem>
+                <FormItem {...formItemLayout} label={"联系电话"}>
+                  {getFieldDecorator('phone', {
+                    rules: [
+                      { required: true, validator: this.validatePhone },
+                    ],
+                  })(<Input />)}
+                </FormItem>
+                <FormItem {...formItemLayout} label={"电子邮箱"}>
+                  {getFieldDecorator('email', {
+                    rules: [
+                      {
+                        required: true,
+                        message: "请输入您的电子邮箱!",
+                      },
+                    ],
+                  })(<Input />)}
+                </FormItem>
+                <FormItem {...formItemLayout} label={"微信绑定"}>
+                  
+                  {(openid && openid != '' && openid != null) ? 
+                    (<>
+                      <a onClick={()=>this.handleBinding('0')}>解绑</a>
+                      <Divider type="vertical" />
+                    </>):""
+                  }
+                  {(openid === '' || openid === null || !openid) ? 
+                    (<>
+                      <a onClick={()=>this.handleBinding('1')}>绑定</a>
+                      <Divider type="vertical" />
+                    </>):""
+                  }
+                  {(openid && openid != '' && openid != null) ? 
+                    (<a onClick={()=>this.handleTest()}>测试</a>):""
+                  }
+                </FormItem>
+                <FormItem style={{display:"none"}}>
+                  {getFieldDecorator('id')(
+                    <Input />
+                  )}
+                </FormItem>
               </Col>
           </Row>
           {
