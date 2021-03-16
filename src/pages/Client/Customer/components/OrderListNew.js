6ab1888a1c7af0838fc4ee3d7e0abb37765ee738
danfuman <1169694155@qq.com>
2021-03-16 16:59:09 +0800

    客户模块

--- src/pages/Client/Customer/components/OrderListNew.js
+++ src/pages/Client/Customer/components/OrderListNew.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'dva';
-import { Form} from 'antd';
+import { Form, Tag } from 'antd';
 
 
 
@@ -61,6 +61,41 @@ class OrderList extends PureComponent {
     })
   }
 
+  // 订单状态
+  getORDERSTATUS = (key) => {
+    let text = ""
+    if(key === 0 || key === '0'){ text = "待审核" }
+    if(key === 1 || key === '1'){ text = "已初审" }
+    if(key === 2 || key === '2'){ text = "已终审" }
+    if(key === 3 || key === '3'){ text = "已发货" }
+    if(key === 4 || key === '4'){ text = "在途中" }
+    if(key === 5 || key === '5'){ text = "已签收" }
+    if(key === 6 || key === '6'){ text = "跟进中" }
+    if(key === 7 || key === '7'){ text = "已激活" }
+    if(key === 11 || key === '11'){ text = "退回中" }
+    if(key === 8 || key === '8'){ text = "已退回" }
+    if(key === 9 || key === '9'){ text = "已取消" }
+    if(key === 10 || key === '10'){ text = "已过期" }
+    return text;
+  }
+  // 订单状态颜色
+  getORDERSCOLOR = (key) => {
+    let text = ""
+    if(key === 0 || key === '0'){ text = "#E6A23C" }
+    if(key === 1 || key === '1'){ text = "#409EFF" }
+    if(key === 2 || key === '2'){ text = "#409EFF" }
+    if(key === 3 || key === '3'){ text = "#409EFF" }
+    if(key === 4 || key === '4'){ text = "#409EFF" }
+    if(key === 5 || key === '5'){ text = "#F56C6C" }
+    if(key === 6 || key === '6'){ text = "#F56C6C" }
+    if(key === 7 || key === '7'){ text = "#67C23A" }
+    if(key === 11 || key === '11'){ text = "#909399" }
+    if(key === 8 || key === '8'){ text = "#909399" }
+    if(key === 9 || key === '9'){ text = "#909399" }
+    if(key === 10 || key === '10'){ text = "#909399" }
+    return text;
+  }
+
   getText = (key, type) => {
     let text = ""
     type.map(item=>{
@@ -105,7 +140,9 @@ class OrderList extends PureComponent {
           ellipsis: true,
           render: (key)=>{
             return (
-              <div>{this.getText(key,ORDERSTATUS)} </div>
+              <Tag color={this.getORDERSCOLOR(key)}>
+                {this.getORDERSTATUS(key)}
+              </Tag>
             )
           }
         },
