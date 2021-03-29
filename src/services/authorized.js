c9ea9b4f2dfadfad89398f99301cdd2738df400c
liup <liuping_love@yeah.net>
2021-03-29 15:51:04 +0800

    公司授权

--- src/services/authorized.js
+++ src/services/authorized.js
@@ -2,14 +2,14 @@ import request from '../utils/request';
 
 //授权查看列表【上级】
 export async function superiorlist(params) {
-  return request('/external/authorized/superiorlist', {
+  return request('/api/external/authorized/superiorlist', {
     method: 'POST',
     body: params,
   });
 }
 //授权查看列表【上级】
 export async function superiorupdate(params) {
-  return request('/external/authorized/superiorupdate', {
+  return request('/api/external/authorized/superiorupdate', {
     method: 'POST',
     body: params,
   });
@@ -17,7 +17,15 @@ export async function superiorupdate(params) {
 
 //授权配置列表【下级】
 export async function subordinateList(params) {
-  return request('/external/authorized/list', {
+  return request('/api/external/authorized/list', {
+    method: 'POST',
+    body: params,
+  });
+}
+
+//新增【下级】
+export async function subordinateSave(params) {
+  return request('/api/external/authorized/save', {
     method: 'POST',
     body: params,
   });
@@ -25,7 +33,7 @@ export async function subordinateList(params) {
 
 //修改【下级】
 export async function subordinateUpdate(params) {
-  return request('/external/authorized/update', {
+  return request('/api/external/authorized/update', {
     method: 'POST',
     body: params,
   });
