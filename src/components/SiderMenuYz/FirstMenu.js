4f3ffe5b16ecd52d4ffd1eb95a593bca374a9a17
liup <liuping_love@yeah.net>
2021-04-28 17:06:10 +0800

    有赞菜单改造

---
+++ src/components/SiderMenuYz/FirstMenu.js
@@ -0,0 +1,153 @@
+import React, { PureComponent, Suspense } from 'react';
+import classNames from 'classnames';
+import { Menu, Icon } from 'antd';
+import Link from 'umi/link';
+import { urlToList } from '../_utils/pathTools';
+import { getMenuMatches } from './SiderMenuUtils';
+import { isUrl } from '@/utils/utils';
+import styles from './index.less';
+import IconFont from '@/components/IconFont';
+import PageLoading from '@/components/PageLoading';
+const SecondtMenu = React.lazy(() => import('./SecondMenu'));
+
+const { SubMenu } = Menu;
+
+// Allow menu.js config icon as string or ReactNode
+//   icon: 'setting',
+//   icon: 'icon-geren' #For Iconfont ,
+//   icon: 'http://demo.com/icon.png',
+//   icon: <Icon type="setting" />,
+const getIcon = icon => {
+  if (typeof icon === 'string') {
+    if (isUrl(icon)) {
+      return <Icon component={() => <img src={icon} alt="icon" className={styles.icon} />}  style={{marginRight:'4px'}}/>;
+    }
+    if (icon.startsWith('icon-')) {
+      return <IconFont type={icon} />;
+    }
+    return <Icon type={icon} style={{marginRight:'4px'}}/>;
+  }
+  return icon;
+};
+
+export default class BaseMenu extends PureComponent {
+
+  /**
+   * 获得菜单子节点
+   * @memberof SiderMenu
+   */
+  getNavMenuItems = (menusData,selectedKeys) => {
+    if (!menusData) {
+      return [];
+    }
+    return menusData
+      .filter(item => item.name && !item.hideInMenu)
+      .map(item => this.getSubMenuOrItem(item,selectedKeys))
+      .filter(item => item);
+  };
+
+  // Get the currently selected menu
+  getSelectedMenuKeys = pathname => {
+    const { flatMenuKeys } = this.props;
+    return urlToList(pathname).map(itemPath => getMenuMatches(flatMenuKeys, itemPath).pop());
+  };
+
+  /**
+   * get SubMenu or Item
+   */
+  getSubMenuOrItem = (item,selectedKeys) => {
+    let current = selectedKeys ? selectedKeys.indexOf(item.path)>-1 ?true:false : false
+    return <li key={item.path} className={ `${styles.li} ${current?styles.active:''} `}>{this.getMenuItemPath(item,current)}</li>;
+  };
+
+  /**
+   * 判断是否是http链接.返回 Link 或 a
+   * Judge whether it is http link.return a or Link
+   * @memberof SiderMenu
+   */
+  getMenuItemPath = (item,current) => {
+    const { name } = item;
+    const itemPath = this.conversionPath(item.path);
+    const icon = getIcon(item.icon);
+    const { target } = item;
+    const { handleOpenChange,collapsed } = this.props;
+
+    const { location, isMobile, onCollapse } = this.props;
+
+    let isView = !collapsed && current
+
+    console.log(isView)
+    console.log(isView)
+    return (
+      <>
+      <Link
+        replace={itemPath === location.pathname}
+        onClick={
+          isMobile
+            ? () => {
+              onCollapse(true);
+            }
+            : undefined
+        }
+      >
+        {icon}
+        <span>{name.substring(0,2)}</span>
+      </Link>
+        <div className={`${styles.secondSidebar} ${isView?styles.view : ''}`}>
+          {item.children?(
+            <Suspense fallback={<PageLoading />}>
+              <SecondtMenu
+                {...this.props}
+                childrens={item.children}
+                titleName={name}
+                mode="inline"
+                handleOpenChange={handleOpenChange}
+                style={{ padding: '16px 0', width: '100%' }}
+              />
+            </Suspense>
+          ):''}
+        </div>
+        </>
+    );
+  };
+
+  conversionPath = path => {
+    if (path && path.indexOf('http') === 0) {
+      return path;
+    }
+    return `/${path || ''}`.replace(/\/+/g, '/');
+  };
+
+  render() {
+    const {
+      openKeys,
+      theme,
+      mode,
+      location: { pathname },
+      className,
+      collapsed,
+    } = this.props;
+
+    let selectedKeys = this.getSelectedMenuKeys(pathname);
+    // if (!selectedKeys.length && openKeys) {
+    //   selectedKeys = [openKeys[openKeys.length - 1]];
+    // }
+
+    const { menuData,style } = this.props;
+    const cls = classNames(className, {
+      'top-nav-menu': mode === 'horizontal',
+    });
+
+    return (
+      <ul
+        key="Menu"
+        mode={mode}
+        theme={theme}
+        style={style}
+        className={cls}
+      >
+        {this.getNavMenuItems(menuData,selectedKeys)}
+      </ul>
+    );
+  }
+}
---
+++ src/components/SiderMenuYz/SiderMenu.js
@@ -0,0 +1,136 @@
+import React, { PureComponent, Suspense } from 'react';
+import { Layout } from 'antd';
+import classNames from 'classnames';
+import Link from 'umi/link';
+import styles from './index.less';
+import PageLoading from '../PageLoading';
+import { getDefaultCollapsedSubMenus } from './SiderMenuUtils';
+import { name } from '../../defaultSettings';
+
+import siderLogo from '../../assets/siderLogo.svg'
+
+// const BaseMenu = React.lazy(() => import('./BaseMenu'));
+const FirstMenu = React.lazy(() => import('./FirstMenu'));
+// const SecondtMenu = React.lazy(() => import('./SecondMenu'));
+const { Sider } = Layout;
+
+let firstMount = true;
+
+export default class SiderMenu extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      openKeys: getDefaultCollapsedSubMenus(props),
+      childrens:null
+    };
+  }
+
+  componentDidMount() {
+    firstMount = false;
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    const { pathname, flatMenuKeysLen } = state;
+    if (props.location.pathname !== pathname || props.flatMenuKeys.length !== flatMenuKeysLen) {
+      return {
+        pathname: props.location.pathname,
+        flatMenuKeysLen: props.flatMenuKeys.length,
+        openKeys: getDefaultCollapsedSubMenus(props),
+      };
+    }
+    return null;
+  }
+
+  isMainMenu = key => {
+    const { menuData } = this.props;
+    return menuData.some(item => {
+      if (key) {
+        return item.key === key || item.path === key;
+      }
+      return false;
+    });
+  };
+
+  handleOpenChange = openKeys => {
+    const moreThanOne = openKeys.filter(openKey => this.isMainMenu(openKey)).length > 1;
+    this.setState({
+      openKeys: moreThanOne ? [openKeys.pop()] : [...openKeys],
+    });
+  };
+
+  handleHover = childrens =>{
+    this.setState({
+      childrens:childrens
+    })
+  }
+
+  render() {
+    const { logo, collapsed, onCollapse, fixSiderbar, theme, isMobile } = this.props;
+    const { openKeys,childrens } = this.state;
+    const defaultProps = collapsed ? {} : { openKeys };
+
+    const siderClassName = classNames(styles.sider, {
+      [styles.fixSiderBar]: fixSiderbar,
+      [styles.light]: theme === 'light',
+    });
+    return (
+      <Sider
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        breakpoint="lg"
+        onCollapse={collapse => {
+          if (firstMount || !isMobile) {
+            onCollapse(collapse);
+          }
+        }}
+        width={92}
+        theme={theme}
+        className={siderClassName}
+      >
+        <div className={styles.firstSidebar}>
+          <div className={styles.logo} id="logo">
+            <Link to="/dashboard/workplace">
+              <img src={siderLogo} alt="logo" />
+              <h1>{name}</h1>
+            </Link>
+          </div>
+          <Suspense fallback={<PageLoading />}>
+            <FirstMenu
+              {...this.props}
+              mode="inline"
+              handleOpenChange={this.handleOpenChange}
+              onOpenChange={this.handleOpenChange}
+              hover = {this.handleHover}
+              style={{ padding: '16px 0', width: '100%' }}
+              {...defaultProps}
+            />
+            {/*<BaseMenu*/}
+            {/*  {...this.props}*/}
+            {/*  mode="inline"*/}
+            {/*  handleOpenChange={this.handleOpenChange}*/}
+            {/*  onOpenChange={this.handleOpenChange}*/}
+            {/*  style={{ padding: '16px 0', width: '100%' }}*/}
+            {/*  {...defaultProps}*/}
+            {/*/>*/}
+          </Suspense>
+        </div>
+        {/*<div>*/}
+        {/*  {childrens?(*/}
+        {/*    <Suspense fallback={<PageLoading />}>*/}
+        {/*      <SecondtMenu*/}
+        {/*        {...this.props}*/}
+        {/*        childrens={childrens}*/}
+        {/*        mode="inline"*/}
+        {/*        handleOpenChange={this.handleOpenChange}*/}
+        {/*        onOpenChange={this.handleOpenChange}*/}
+        {/*        style={{ padding: '16px 0', width: '100%' }}*/}
+        {/*        {...defaultProps}*/}
+        {/*      />*/}
+        {/*    </Suspense>*/}
+        {/*  ):''}*/}
+        {/*</div>*/}
+      </Sider>
+    );
+  }
+}
